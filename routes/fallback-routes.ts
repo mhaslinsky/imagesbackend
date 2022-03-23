51f794102553bd22e16040351de1d164da7111d7
mhaslinsky <mhaslinsky@gmail.com>
2022-03-23 17:19:27 -0400

    welcome route to API for health checks

--- routes/fallback-routes.ts
+++ routes/fallback-routes.ts
@@ -4,7 +4,7 @@ import express from "express";
 const fallBackRouter = express.Router();
 
 fallBackRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.status(204).json({ message: "instaSham API" });
+  res.status(200).json({ message: "instaSham API" });
 });
 
 export default fallBackRouter;
