3e4b69276842e26bc4c5115e47d406fdaa150779
mhaslinsky <mhaslinsky@gmail.com>
2022-04-17 21:03:36 -0400

    added nginx.conf file

--- util/build.js
+++ util/build.js
@@ -2,7 +2,13 @@ const fs = require("fs-extra");
 const childProcess = require("child_process");
 
 try {
-  fs.removeSync("/dist");
+  fs.removeSync("/dist/controllers");
+  fs.removeSync("/dist/middleware");
+  fs.removeSync("/dist/models");
+  fs.removeSync("/dist/routes");
+  fs.removeSync("/dist/util");
+  fs.removeSync("/dist/app.js");
+  fs.removeSync("/dist/app.js.map");
 
   childProcess.exec("tsc --build tsconfig.json");
 } catch (err) {
