ca38becebb892c907e842329c7460e1fa22b24f0
mhaslinsky <mhaslinsky@gmail.com>
2022-04-21 16:28:04 -0400

    changing build process to move nginx config over

--- util/build.js
+++ util/build.js
@@ -3,8 +3,8 @@ const childProcess = require("child_process");
 
 try {
   fs.removeSync("/dist");
-
   childProcess.exec("tsc --build tsconfig.json");
+  fs.copy(`./nginx`, `./dist`);
 } catch (err) {
   console.log(err);
 }
