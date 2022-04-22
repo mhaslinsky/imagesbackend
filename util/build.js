13dfae6d975fb8e7de4a1cc9918cabd306021265
mhaslinsky <mhaslinsky@gmail.com>
2022-04-22 17:13:42 -0400

    nginx config

--- util/build.js
+++ util/build.js
@@ -5,7 +5,7 @@ try {
   fs.removeSync("/dist");
   console.log("deleting dist folder");
   childProcess.exec("tsc --build tsconfig.json");
-  fs.copySync(`./nginx`, `./`);
+  fs.copySync(`./nginx`, `./current`);
 } catch (err) {
   console.log(err);
 }
